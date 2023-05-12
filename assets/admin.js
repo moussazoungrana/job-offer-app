7cd624da6d787b7874bf71a68d82eb6caff304b0
Moussa ZOUNGRANA <moussawalidzoungrana@gmail.com>
2023-05-12 20:05:44 +0100

    clean

--- assets/admin.js
+++ assets/admin.js
@@ -7,7 +7,6 @@ import 'tom-select/dist/css/tom-select.bootstrap4.min.css';
 document.addEventListener('DOMContentLoaded',function () {
     Array.from(document.querySelectorAll('select[multiple]')).forEach((select) => {
         new TomSelect(select, {
-            create: true,
             createFilter: function(input) { return input.length >= 3 }
         })
     })
